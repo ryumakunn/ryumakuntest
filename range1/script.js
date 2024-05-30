8957a994e31d844e377bf8369abbcd604ff7d05a
ryumakunn <154520574+ryumakunn@users.noreply.github.com>
2024-05-30 10:26:25 +0900

    Update script.js

--- range1/script.js
+++ range1/script.js
@@ -11,7 +11,7 @@ window.addEventListener('deviceorientation', (event) => {
     const gamma = event.gamma; // Y軸 (-90から90度)
 
     document.getElementById('orientation').innerText =
-        `Alpha (Z軸): ${alpha.toFixed(2)}°\n` +
+        `Alpha (Z軸): ${alpha.toFixed(1)}°\n` +
         `Beta (X軸): ${beta.toFixed(2)}°\n` +
         `Gamma (Y軸): ${gamma.toFixed(2)}°`;
 });
