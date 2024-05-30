be93d47ac755b30f696a409c1c3183fb077fdc08
ryumakunn <154520574+ryumakunn@users.noreply.github.com>
2024-05-30 10:46:20 +0900

    Update script.js

--- range1/script.js
+++ range1/script.js
@@ -1,20 +1,15 @@
 // script.js
 
-let currentHeading = 0;
-let initialNorth = 0;
+let set = 0;
+let defo = 0;
 
 document.getElementById('setNorth').addEventListener('click', () => {
-    initialNorth = currentHeading;
     alert('北を設定しました');
 });
 
 window.addEventListener('deviceorientation', (event) => {
     const alpha = event.alpha; // Z軸 (0から360度)
-    const beta = event.beta;   // X軸 (-180から180度)
-    const gamma = event.gamma; // Y軸 (-90から90度)
 
     document.getElementById('orientation').innerText =
-        `Alpha (Z軸): ${alpha.toFixed(1)}°\n` +
-        `Beta (X軸): ${beta.toFixed(2)}°\n` +
-        `Gamma (Y軸): ${gamma.toFixed(2)}°`;
+        `Alpha (Z軸): ${alpha.toFixed(2)}°\n` ;
 });
