3c33663e89738e9d136897a4c8a818e25ef2271c
ryumakunn <154520574+ryumakunn@users.noreply.github.com>
2024-05-30 10:59:00 +0900

    Update script.js

--- range1/script.js
+++ range1/script.js
@@ -4,12 +4,12 @@ let defo = 0;
 
 document.getElementById('setNorth').addEventListener('click', () => {
     alert('北を設定しました');
-    const set = event.alpha;
+    console.log((set=alpha));
 });
 
 window.addEventListener('deviceorientation', (event) => {
-    const lpha = event.alpha; // Z軸 (0から360度)
-    const alpha = lpha-set;
+    const alpha = event.alpha; // Z軸 (0から360度)
+    console.log((alpha=alpha-set));
 
     document.getElementById('orientation').innerText =
         `Alpha (Z軸): ${alpha.toFixed(2)}°\n` ;
